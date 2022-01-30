73d826b46731a74dc573f1c99a97c6030b97fa08
moom1315 <age39303@gmail.com>
2022-01-30 11:24:15 -0800

    sound button script

---
+++ Landlord/Assets/Scripts/SoundButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour
+{
+    public Sprite unmuteSprite;
+    public Sprite muteSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Onclick()
+    {
+        if (this.GetComponent<Image>().sprite == unmuteSprite)
+        {
+            this.GetComponent<Image>().sprite = muteSprite;
+        }
+        else
+        {
+            this.GetComponent<Image>().sprite = unmuteSprite;
+        }
+    }
+}
