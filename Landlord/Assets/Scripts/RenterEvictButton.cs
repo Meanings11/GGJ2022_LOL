62660adac88037dccf38066047169285a414b456
Barry Yip <ys5851@163.com>
2022-01-30 14:36:44 -0800

    delete evict popup

--- Landlord/Assets/Scripts/RenterEvictButton.cs
+++ Landlord/Assets/Scripts/RenterEvictButton.cs
@@ -8,68 +8,34 @@ public class RenterEvictButton : MonoBehaviour
     public Image mask;
     public GameObject Renter;
 
-    public Text name;
+    public Text nameText;
+    public Text happinessText;
 
-    public CanvasGroup confirmationCanvasGroup;
     public CanvasGroup selfCanvasGroup;
 
     private static float TIME_DURATION = 0.25f;
 
     private void Start()
     {
-        confirmationCanvasGroup.gameObject.SetActive(false);
         selfCanvasGroup.gameObject.SetActive(false);
     }
 
     public void OpenPopup()
     {
         mask.color = new Color(0f, 0f, 0f, 0.5f);
-        name.text = Renter.GetComponent<Renter>().name;
-        PopupWindow(selfCanvasGroup);
-    }
-
-    void PopupWindow(CanvasGroup canvasGroup)
-    {
-        canvasGroup.gameObject.SetActive(true);
-        canvasGroup.DOFade(1, TIME_DURATION).OnComplete(() =>
+        nameText.text = Renter.GetComponent<Renter>().name;
+        happinessText.text = Renter.GetComponent<Renter>().happiness + "%";
+        selfCanvasGroup.gameObject.SetActive(true);
+        selfCanvasGroup.DOFade(1, TIME_DURATION).OnComplete(() =>
         {
-            canvasGroup.interactable = true;
+            selfCanvasGroup.interactable = true;
         });
     }
-
-    public void EvictOnClick()
-    {
-        CloseWindow(selfCanvasGroup);
-        // PopupWindow(confirmationCanvasGroup);
-    }
-
-    void CloseWindow(CanvasGroup cg)
-    {
-        cg.interactable = false;
-        cg.alpha = 0;
-    }
-
-    public void YesOnClick()
-    {
-        CloseWindow(confirmationCanvasGroup);
-        if (Renter != null)
-        {
-            Renter.transform.parent.gameObject.SetActive(false);
-        }
-
-        ClosePopup();
-    }
-
-    public void NoOnClick()
-    {
-        CloseWindow(confirmationCanvasGroup);
-        selfCanvasGroup.alpha = 1;
-        selfCanvasGroup.interactable = true;
-    }
-
+    
     public void ClosePopup()
     {
-        CloseWindow(selfCanvasGroup);
+        selfCanvasGroup.interactable = false;
+        selfCanvasGroup.alpha = 0;
         mask.color = new Color(0f, 0f, 0f, 0f);
     }
 }
